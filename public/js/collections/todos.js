740a2a5a586dcadb3a22e54a7cb9ce0d1abcf382
Clinton N. Dreisbach <clinton@dreisbach.us>
2014-11-05 09:33:34 -0500

    Added Backbone-JSON-API

--- public/js/collections/todos.js
+++ public/js/collections/todos.js
@@ -14,7 +14,7 @@ var app = app || {};
 		model: app.Todo,
 
 		// Save all of the todo items under the `"todos"` namespace.
-		url: '/api/v1/notes',
+		url: '/api/v1/todos',
 
 		// Filter down the list of all todo items that are finished.
 		completed: function () {
